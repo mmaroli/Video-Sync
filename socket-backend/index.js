9d61046c145ee3870de16792ad4b47f224954697
Manav Maroli <mmaroli@ucla.edu>
2020-06-03 22:33:02 -0700

    Add dockerfile and index.js

---
+++ socket-backend/index.js
@@ -0,0 +1,55 @@
+
+const fs = require('fs');
+
+const server = require('http').createServer(handler);
+const io = require('socket.io')(server)
+
+server.listen(80);
+
+
+// -------------------------------------------------
+// Serving Client Code
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end("error loading index.html");
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+// -------------------------------------------------
+
+
+
+
+
+
+
+io.on('connection', (socket) => {
+  console.log("User has connected");
+
+  socket.on('create-nickname', (nickname) => {
+    socket.nickname = nickname
+  });
+
+  socket.on('join-room', (room_id) => {
+    socket.join(room_id, () => {
+      let rooms = Object.keys(socket.rooms);
+      console.log(rooms);
+      io.to(room_id).emit("new-user", 'hello', socket.nickname)
+    });
+  });
+
+
+
+
+  socket.on('disconnect', () => {
+    console.log("User has disconnected");
+  })
+});
